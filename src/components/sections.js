fc91b08d0f97937f4b808bbc9a0ca741ab26412e
Артур <artur8888b@gmail.com>
2019-11-28 21:42:25 +0200

    Выполнено задание

---
+++ src/components/sections.js
@@ -0,0 +1,5 @@
+export const createTripEventForm = () =>
+  `<form class="trip-events__item  event  event--edit" action="#" method="post"></form>`;
+
+export const createEventDetails = () =>
+  `<section class="event__details"></section>`;
