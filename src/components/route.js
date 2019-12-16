8c43dfd5092d8b6d7db21d123897eb817c978878
Артур <artur8888b@gmail.com>
2019-12-16 19:51:02 +0200

    Задание 3. Структуры данных Личный проект: шаблонизируй это

--- src/components/route.js
+++ src/components/route.js
@@ -1,6 +1,31 @@
-export const createRoute = () =>
-  `<div class="trip-info__main">
-  <h1 class="trip-info__title">Amsterdam &mdash; ... &mdash; Amsterdam</h1>
+export const createRoute = (routeData) => {
+
+  let tripDaysItems = routeData;
+  let routeMarkup = ``;
+  let cities = tripDaysItems.map((it) => it.city);
+  for (const cityName of cities) {
+    routeMarkup += cityName + ` &mdash; `;
+  }
+
+  routeMarkup = cities.length > 2 ?
+    `${cities[0]}` + ` ... ` + `${cities[cities.length - 1]}` :
+    routeMarkup.substring(0, routeMarkup.length - 9);
+
+  return `<div class="trip-info__main">
+  <h1 class="trip-info__title">${routeMarkup}</h1>
 
   <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;21</p>
 </div>`;
+};
+
+export const calculateFullTripExpenses = (pricesData) => {
+
+  let totalTripRate = 0;
+  const priceOutput = document.querySelector(`.trip-info__cost-value`);
+  const totalTriPrices = Array.from(pricesData).map((it) => it.textContent);
+  totalTriPrices.forEach((it) => {
+    totalTripRate = totalTripRate + parseInt(it, 10);
+  });
+  priceOutput.textContent = totalTripRate;
+
+};
---
+++ src/mocks/route-point.js
@@ -0,0 +1,110 @@
+import {ROUTE_POINTS_TYPES, CITIES, MONTH_NAMES, ADDITIONAL_OPTIONS} from '../const.js';
+
+
+const descriptionText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Cras aliquet varius magna, non porta ligula feugiat eget.
+Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra.
+Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris,
+condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus,
+purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis.
+Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
+
+const PICTURES_QTY = 5;
+const monthDaysQty = 31;
+const generatePictureURL = () => `http://picsum.photos/300/150?r=${Math.random()}`;
+const minutesQty = 60;
+const hoursQty = 23;
+
+const generatePictures = (count) => {
+  return new Array(count).fill(``)
+    .map(generatePictureURL);
+};
+
+const getRandomIntegerNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const getDescription = (text) => {
+  let textArray = text.split(`. `);
+  let sentenceQty = getRandomIntegerNumber(1, 3);
+  let description = ``;
+  for (let i = 0; i < sentenceQty; i++) {
+    description += textArray[getRandomIntegerNumber(0, textArray.length)] + `. `;
+  }
+
+  return description;
+};
+
+const getRandomDate = () => {
+  const randomStartHours = getRandomIntegerNumber(1, hoursQty);
+  const randomStartMinutes = getRandomIntegerNumber(1, minutesQty);
+  const startTimeHours = randomStartHours < 10 ? `0` + randomStartHours : randomStartHours;
+  const startTimeMinutes = randomStartMinutes < 10 ? `0` + `${randomStartMinutes}` : randomStartMinutes;
+  const startTime = `${startTimeHours}:${startTimeMinutes}`;
+
+  const randomEndHours = getRandomIntegerNumber(parseInt(startTimeHours, 10), hoursQty);
+  const randomEndMinutes = getRandomIntegerNumber(1, minutesQty);
+  const endTimeHours = randomEndHours < 10 ? `0` + randomEndHours : randomEndHours;
+  const endTimeMinutes = randomEndMinutes < 10 ? `0` + `${randomEndMinutes}` : randomEndMinutes;
+  const endTime = `${endTimeHours}:${endTimeMinutes}`;
+
+  let eventDurationHours = endTimeHours - startTimeHours;
+  eventDurationHours = eventDurationHours < 0 ? 24 + eventDurationHours : eventDurationHours;
+
+  let eventDurationMinutes = endTimeMinutes - startTimeMinutes;
+  eventDurationMinutes = eventDurationMinutes < 0 ? minutesQty + eventDurationMinutes : eventDurationMinutes;
+
+  const eventDurationTime = `${eventDurationHours + `H`} ${eventDurationMinutes + `M`}`;
+
+  return {
+    'year': new Date().getFullYear(),
+    'month': MONTH_NAMES[getRandomIntegerNumber(0, MONTH_NAMES.length)],
+    'day': getRandomIntegerNumber(1, monthDaysQty),
+    startTime,
+    endTime,
+    eventDurationTime
+  };
+};
+
+
+const generateRoutePoint = () => {
+
+  const optionsQty = getRandomIntegerNumber(1, 4);
+  const optionsInfo = [];
+
+  for (let i = 0; i < optionsQty; i++) {
+    optionsInfo.push(ADDITIONAL_OPTIONS[getRandomIntegerNumber(0, ADDITIONAL_OPTIONS.length)]);
+  }
+
+  const getRandomType = () => {
+    const randomTypeKey = Math.random() > 0.5 ? `ride` : `stops`;
+    const prefix = randomTypeKey === `ride` ? `to` : `in`;
+    const randomTypeArray = Object.entries(ROUTE_POINTS_TYPES[randomTypeKey]);
+    const randomTypeElement = randomTypeArray[getRandomIntegerNumber(0, randomTypeArray.length)];
+
+
+    return {
+      'travelType': randomTypeElement[0],
+      'icon': randomTypeElement[1],
+      prefix
+    };
+  };
+
+  return {
+    'type': getRandomType(),
+    'city': CITIES[getRandomIntegerNumber(0, CITIES.length)],
+    'pictures': generatePictures(PICTURES_QTY),
+    'description': getDescription(descriptionText),
+    'price': getRandomIntegerNumber(10, 1000),
+    'date': getRandomDate(),
+    'options': optionsInfo
+  };
+};
+
+const generateRoutePoints = (count) => {
+  return new Array(count)
+    .fill(``)
+    .map(generateRoutePoint);
+};
+
+export {generateRoutePoint, generateRoutePoints};
