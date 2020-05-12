60d71602d1738d926ba5ce11cc5d2919b91f9d4e
Артур <artur8888b@gmail.com>
2020-05-12 11:26:05 -0400

    Исправлены методы фильтрации

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -1,42 +1,30 @@
 import {filterTypes} from '../const.js';
-import moment from 'moment';
-
 
 export const getFutureRoutePoints = (nowDate, points) => {
 
-  const nowDateInMiliseconds = moment(nowDate, `DD/MM/YYYY HH:mm`).valueOf();
-
   return points.filter((it) => {
-    const futureDateInMiliseconds = moment(it.endDate).valueOf();
-    console.log(futureDateInMiliseconds > nowDateInMiliseconds)
-    return futureDateInMiliseconds > nowDateInMiliseconds;
+    return it.startDate > nowDate;
   });
 };
 
 export const gePastRoutePoints = (nowDate, points) => {
 
-  const nowDateInMiliseconds = moment(nowDate, `DD/MM/YYYY HH:mm`).valueOf();
   return points.filter((it) => {
-    const pastDateInMiliseconds = moment(it.startDate).valueOf();
-    console.log(pastDateInMiliseconds < nowDateInMiliseconds)
-    return pastDateInMiliseconds < nowDateInMiliseconds;
-
+    return it.startDate < nowDate;
   });
 };
 
-
 export const getPointsByFilter = (points, filterType) => {
   const nowDate = new Date();
-
   switch (filterType) {
     case filterTypes[`filter-everything`].name:
       return points;
-    case filterTypes[`filter-future`].name:
-      return getFutureRoutePoints(nowDate, points);
+
     case filterTypes[`filter-past`].name:
       return gePastRoutePoints(nowDate, points);
-  }
 
+    case filterTypes[`filter-future`].name:
+      return getFutureRoutePoints(nowDate, points);
+  }
   return points;
 };
-
